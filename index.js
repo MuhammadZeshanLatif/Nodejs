a436b42bb865f9dc7d3d2c6ac7a2b77b4b702a38
Muhammad Zeeshan Latif <zeeshanmuhammad42201@gmail.com>
2023-10-20 00:34:04 +0500

    a

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
 const { json } = require('express');
 const fs=require('fs');
 const http = require('http');
-const data=fs.readFileSync('data.json','utf-8');
+const data=JSON.parse(fs.readFileSync('data.json','utf-8'));
 const index=fs.readFileSync('index.html','utf-8');
 const errorPage=fs.readFileSync('error.html','utf-8');
+let movieData=data[0];
+
 //make server 
 const server= http.createServer((req,res)=>{
     console.log("Server Started...");
     switch(req.url){
         case '/index':
             res.setHeader('Content-Type','text/html')
-            res.end(index);
-            break;
+            let modifiedindex= index.replace('**title**',movieData.movie).replace('**images**',movieData.image);
+            res.end(modifiedindex);
         case "/data":
             res.setHeader('Content-Type','application/json')
-            res.end(data);
-            break;
+            res.end(JSON.stringify(data));
+            break;    
         default:
             res.setHeader('Content-Type','text/html')
             res.end(errorPage);
             break;
-            // res.writeHead(404,'')
-            // res.end();
 
     }
 });
